dc34177a9e408f9aa41436284b1799a37da395db
Pranjal Gain <pranjalgain052@gmail.com>
2025-05-21 02:42:40 +0530

    feat: Update base_url to point to the production environment

--- frontend/src/constants/constants.js
+++ frontend/src/constants/constants.js
@@ -1,2 +1,3 @@
-export const base_url = `http://localhost:3000`
-export const documentTitle = `School Management`
+// export const base_url = `http://localhost:3000`
+export const base_url = `https://school-management-ten-indol.vercel.app`;
+export const documentTitle = `School Management`;
